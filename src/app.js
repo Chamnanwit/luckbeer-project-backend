2727e7278b1ba37cfe27499348203f15b2087a4e
Chamnanwit <Chamnanwiteamdit@gmail.com>
2023-05-28 22:11:18 +0700

    make middleware error and notfound

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+
+const app = express();
+
+if (process.env.NODE_ENV === 'develop') {
+    app.use(morgan("combined"))
+};
+
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+app.listen(process.env.PORT || 8000, () =>
+  console.log("server running on port " + process.env.PORT)
+);
